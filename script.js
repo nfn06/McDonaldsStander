793828cd8305665ac82764351e8f7cd3d33d8bc9
nfn06 <noahfnielsen11@gmail.com>
2025-10-29 14:45:43 +0100

    feat: added basic price functionality

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ orderBtn.addEventListener('click', () => {
     welcomeDiv.style.display = 'none';
     menu.style.display = 'flex';
 })
+
+let basketValue = 0;
+const resultDisplay = document.getElementById('result');
+
+const addButtons = document.querySelectorAll('.addBtn');
+addButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const price = parseFloat(button.getAttribute('data-price'));
+        basketValue += price;
+        resultDisplay.textContent = `Basket Value: ${basketValue} DKK`;
+    });
+});
+
